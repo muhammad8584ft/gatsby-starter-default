194de6f9a3cdc0ec38223c449848b1c4afc4028c
muhammad8584ft <91459241+muhammad8584ft@users.noreply.github.com>
2023-05-25 14:34:05 +0700

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,1 +1,25 @@
+import React from "react"
+import { graphql } from "gatsby"
 
+const PostTemplate = ({ data }) => {
+  const post = data.markdownRemark
+  return (
+    <div>
+      <h1>{post.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  )
+}
+
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`
+
+export default PostTemplate
